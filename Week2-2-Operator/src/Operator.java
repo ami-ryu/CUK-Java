dbe1bbf3af99dd713b2e1ae7aebe2d752e327907
Ami Ryu <ami@levvels.io>
2023-12-25 19:39:27 +0900

    Operator

---
+++ Week2-2-Operator/src/Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+	public static void main(String[] args) {
+		int a = 3, b = 2;
+		System.out.println("a % b = " + (a % b));
+		System.out.println("a != b = " + (a != b));
+		System.out.println((a == b) && (a == 5));
+		System.out.println(b *= a);
+		System.out.println("++a => " + (++a));
+	}
+}
