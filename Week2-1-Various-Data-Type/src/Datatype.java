ba4b9e3a547c27dd5dae64d158e2ce7cc431f6ba
Ami Ryu <ami@levvels.io>
2023-12-25 19:33:35 +0900

    Datatype

---
+++ Week2-1-Various-Data-Type/src/Datatype.java
@@ -0,0 +1,14 @@
+public class Datatype {
+	public static void main(String[] args) {
+		int num1 = 100000;
+		float num2 = 12.34f;
+		char ch = 'A';
+		String name = "홍길동";
+		boolean flag = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(flag);
+	}
+}
